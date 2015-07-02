c689b928ca6b04d7360c4ee00b4f2bd7e84fbf92
alex <alex.castaneda@gmail.com>
2015-07-02 13:32:46 -0700

    add error message test

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -11,4 +11,15 @@ describe "the add a project process" do
     click_on 'Create Project'
     expect(page).to have_content 'Blah Blah'
   end
+
+  it "gives error message when not field not filled in" do
+    skill = Skill.create(:name => 'This Sucks', :description => 'Wish I had read the instructions better')
+    visit skill_path(skill)
+    click_on 'Add a Project'
+    fill_in 'Name', :with => 'Blah Blah'
+    fill_in 'Description', :with => 'Still stupid'
+    click_on 'Create Project'
+    expect(page).to have_content "Sorry, you've made the following errors"
+  end
+
 end
--- spec/features/add_skill_spec.rb
+++ spec/features/add_skill_spec.rb
@@ -9,4 +9,13 @@ describe "the add a skill process" do
     click_on 'Create Skill'
     expect(page).to have_content 'Skill 1'
   end
+
+  it "gives error message when not field not filled in" do
+    visit skills_path
+    click_on 'Add Even More Skills'
+    fill_in 'Name', :with => 'Skill 1'
+    click_on 'Create Skill'
+    expect(page).to have_content "Sorry, you've made the following errors"
+  end
+
 end
