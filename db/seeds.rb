d69303d9e9b264bdd46ddb5216c40e542778965e
alexcaste <alex.castaneda@gmail.com>
2015-07-10 16:48:46 -0700

    add heroku link and admin seed for future use

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first_name: 'Badass', last_name: 'Mofo', email: 'mofo@badass.com', password: 'badassmofo', admin: true)
